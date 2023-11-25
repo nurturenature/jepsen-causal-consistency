d2fceca1a530d2c20d9544d1fb6fe532a1292beb
NurtureNature <jeff.m.suttor@gmail.com>
2023-11-24 19:43:16 -0800

    initial read your writes graph

--- src/crdt/util.clj
+++ src/crdt/util.clj
@@ -1,5 +1,12 @@
 (ns crdt.util
-  (:require [jepsen.history :as h]))
+  (:require [clojure.set :as set]
+           [clojure.tools.logging :refer [info]]
+           [elle
+            [core :as ec]
+            [graph :as g]]
+           [jepsen.history :as h]
+           [jepsen.history.fold :as f]
+           [slingshot.slingshot :refer [try+ throw+]]))
 
 (defn gset-history
   "Generate a sample history of gset ops.
@@ -24,3 +31,91 @@
                    :f :r
                    :value (set (range (+ 1 v)))}]))
           (h/history)))))
+
+(defn gset-ryw-anomaly-history
+  "Generate a sample history of gset ops with a read-your-writes anomaly."
+  []
+  (->> [{:process 0
+         :type :ok
+         :f :a
+         :value 0}
+        {:process 0
+         :type :ok
+         :f :r
+         :value #{}}]
+       (h/history)))
+
+(defrecord RYWExplainer []
+  ec/DataExplainer
+  (explain-pair-data [_ a b]
+    (when (and (= (.process a) (.process b))
+               ((h/has-f? :r) a)
+               ((h/has-f? :a) b)
+               (not (contains? (.value a) (.value b))))
+      {:type    :read-your-writes
+       :process (.process a)
+       :read    (.value a)
+       :add     (.value b)}))
+
+  (render-explanation [_ {:keys [process read add]} a-name b-name]
+    (str "In process " process " " a-name " read of " (pr-str read)
+         " did not observe " b-name " add of " (pr-str add))))
+
+(defn ryw-graph
+  "Given a history, builds a graph of rw-antidependency relationships within each process.
+   A ryw anomaly will cycle rw-antidependency and process order graphs."
+  [history]
+  (let [history (->> history
+                     (h/client-ops)
+                     (h/oks))
+        adds  (->> history (h/filter-f :a))
+        reads (->> history (h/filter-f :r))
+        adds-by-process-fold (f/make-fold {:name :adds-by-process
+                                           :reducer-identity (constantly {})
+                                           :reducer (fn [acc op]
+                                                      (let [process (.process op)
+                                                            value (.value op)]
+                                                        (update acc process
+                                                                (fn [old value]
+                                                                  (if (nil? old)
+                                                                    #{value}
+                                                                    (conj old value)))
+                                                                value)))
+                                           :combiner-identity (constantly {})
+                                           :combiner (fn [acc chunk]
+                                                       (merge-with set/union
+                                                                   acc chunk))})
+        add-by-value-fold (f/make-fold {:name :add-by-value
+                                        :reducer-identity (constantly {})
+                                        :reducer (fn [acc op]
+                                                   (let [value (.value op)]
+                                                     (assoc acc value op)))
+                                        :combiner-identity (constantly {})
+                                        :combiner merge})
+        fused_folds (f/fuse adds-by-process-fold add-by-value-fold)
+        [adds-by-process
+         add-by-value]   (-> adds
+                             (h/fold (:fused fused_folds)))
+        graph (->> reads
+                   (reduce (fn [g op]
+                             (let [process (.process op)
+                                   value   (.value op)
+                                   unread  (set/difference (get adds-by-process process) value)
+                                   first-unread (->> unread
+                                                     (apply min) ; add values are monotonic per process
+                                                     (get add-by-value))]
+                               (g/link g op first-unread)))
+                           (g/linear (g/named-graph :read-your-writes (g/digraph)))) ; TODO: op-digraph
+                   g/forked)]
+
+
+
+    {:graph     graph
+     :explainer (RYWExplainer.)}))
+
+(defn ryw-check
+  "Checks a history for read your writes anomalies."
+  [history]
+  (->> history
+       (ec/check {:analyzer (ec/combine ryw-graph ec/process-graph)
+                  :directory "./target/out"})))
