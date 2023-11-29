fe754ce85e08536395a09fc889830850341aa750
NurtureNature <jeff.m.suttor@gmail.com>
2023-11-28 17:54:14 -0800

    test for  ryw, mono-w, mono-r

--- test/crdt/gset_test.clj
+++ test/crdt/gset_test.clj
@@ -2,6 +2,28 @@
   (:require [clojure.test :refer :all]
             [crdt.gset :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-history
+  (testing "basic gset history"
+    (is (:valid? (->> (gset-history)
+                      causal-check)))))
+
+(deftest read-your-writes
+  (testing "read your writes"
+    (is (:valid? (->> gset-ryw-history
+                      causal-check)))
+    (is (not (:valid? (->> gset-ryw-anomaly-history
+                           causal-check))))))
+
+(deftest monotonic-writes
+  (testing "monotonic writes"
+    (is (:valid? (->> gset-mono-w-history
+                      causal-check)))
+    (is (not (:valid? (->> gset-mono-w-anomaly-history
+                           causal-check))))))
+
+(deftest monotonic-reads
+  (testing "monotonic reads"
+    (is (:valid? (->> gset-mono-r-history
+                      causal-check)))
+    (is (not (:valid? (->> gset-mono-r-anomaly-history
+                           causal-check))))))
