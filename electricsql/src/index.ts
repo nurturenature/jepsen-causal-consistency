6d097e7c735f9e066b5720516053aebc6a3e6165
nurturenature <jeff.suttor@earthlink.net>
2023-12-27 22:03:42 -0800

    client: electricsql migrations, r/w from the database, simple REST API

--- electricsql/src/index.ts
+++ electricsql/src/index.ts
@@ -3,11 +3,63 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+/* ElectricSQL */
+import Database from 'better-sqlite3'
+import { electrify } from 'electric-sql/node'
+import { schema } from './generated/client/index.js'
+
+import jwt from 'jsonwebtoken'
+function unsignedJWT(sub: string, customClaims?: object) {
+    const claims = { "iat": 1703740031, "exp": 1803743631 } || {}
+
+    return jwt.sign({ ...claims, sub: sub }, '', { algorithm: 'none' })
+}
+const config = {
+    url: "http://electricsql:5133",
+    auth: {
+        token: unsignedJWT("insecure")
+    },
+    debug: true
+}
+
+const conn = new Database('electric.db')
+conn.pragma('journal_mode = WAL')
+const electric = await electrify(conn, schema, config)
+const { db } = electric
+const shape = await db.lww_registers.sync()
+await shape.synced
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+app.get("/r/:key", async (req: Request, res: Response) => {
+    const value = await db.lww_registers.findUnique({
+        where: {
+            key: parseInt(req.params.key, 10)
+        }
+    })
+    res.send(value);
+});
+
+app.get("/list", async (req: Request, res: Response) => {
+    const result = await db.lww_registers.findMany()
+    res.send(result);
+});
+
+app.get("/w/:key/:value", async (req: Request, res: Response) => {
+    const result = await db.lww_registers.upsert({
+        create: {
+            key: parseInt(req.params.key, 10),
+            value: parseInt(req.params.value, 10)
+        },
+        update: {
+            value: parseInt(req.params.value, 10)
+        },
+        where: {
+            key: parseInt(req.params.key, 10)
+        }
+    })
+    res.send(result);
 });
 
 app.listen(port, () => {
