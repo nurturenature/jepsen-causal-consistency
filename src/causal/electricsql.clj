f2300772aa4c55a084f7ee0d4b55fa0d001b283e
nurturenature <jeff.suttor@earthlink.net>
2023-12-28 23:18:40 -0800

    more robust setup, teardown, delete of PostgreSQL and ElectricSQL nodes

--- src/causal/electricsql.clj
+++ src/causal/electricsql.clj
@@ -6,8 +6,10 @@
             [jepsen
              [control :as c]
              [util :as u]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]
             [slingshot.slingshot :refer [try+]]
-            [jepsen.os.debian :as deb]))
+            [jepsen.control :as c]))
 
 (def host
   "Name of host machine for ElectricSQL."
@@ -21,14 +23,164 @@
   "PostgreSQL proxy password."
   postgresql/electric-password)
 
+(def connection-url
+  "ElectricSQL connection URI."
+  (str "postgresql://" postgresql/electric-user ":" pg-proxy-password "@" host ":" pg-proxy-port "/electric"))
+
 (def install-dir
   "Directory to install ElectricSQL to."
   "/root/electricsql")
 
+(def bin
+  "ElectricSQL binary."
+  (str install-dir "/components/electric/_build/prod/rel/electric/bin/electric"))
+
+(def bin-env
+  "Environment vars to start `bin` with."
+  (c/env {:DATABASE_URL           postgresql/connection-url
+          :LOGICAL_PUBLISHER_HOST host
+          :PG_PROXY_PORT          pg-proxy-port
+          :PG_PROXY_PASSWORD      pg-proxy-password
+          :AUTH_MODE              :insecure}))
+
+(def run-elixir
+  "Helper script to run elixir apps."
+  "/root/elixir/run-elixir.sh")
+
+(defn run-elixir-sh
+  "Creates /root/elixir/run-elixir.sh helper script.
+   Assumes `c/on c/su`"
+  []
+  (c/exec :rm :-f run-elixir)
+  (c/exec :echo "export PATH=\"/root/elixir/bin:$PATH\""
+          :>> run-elixir)
+  (c/exec :echo "\"$@\""
+          :>> run-elixir)
+  (c/exec :chmod :u+x run-elixir))
+
+(defn insure-psql
+  "Insures psql is installed.
+   Assumes `c/on c/su`."
+  []
+  (when-not (try+
+             (c/exec :psql :--version)
+             true
+             (catch [] _
+               false))
+    (postgresql/insure-repo)
+    (debian/install [:postgresql-client])))
+
+(defn teardown
+  "Teardown ElectricSQL."
+  [_opts]
+  (info "Tearing down ElectricSQL")
+  (c/on host
+        (c/su
+         ; tests may have stopped/killed service
+         (when (try+
+                (c/exec bin-env run-elixir bin :restart)
+                true
+                (catch [] _
+                  false))
+           ; un-electrify, drop table
+           (u/meh
+            (c/exec :psql :-d connection-url
+                    :-c "ALTER TABLE public.lww_registers DISABLE ELECTRIC;"))
+           (u/meh
+            (c/exec :psql :-d connection-url
+                    :-c "DROP TABLE public.lww_registers;"))
+           (info "ElectricSQL tables: "
+                 (c/exec :psql :-d connection-url
+                         :-c "\\dt"))
+
+           ; stop service 
+           (c/exec bin-env run-elixir bin :stop)))))
+
+(defn delete
+  "Delete ElectricSQL."
+  [_opts]
+  (info "Deleting ElectricSQL")
+  (c/on host
+        (c/su
+         (u/meh
+          (c/exec bin-env bin :stop))
+         (u/meh
+          (c/exec :rm :-rf install-dir)))))
+
+(defn setup
+  "Sets up, installing if necessary, starts, configures ElectricSQL"
+  [_opts]
+  (info "Setting up ElectricSQL on " host)
+  (c/on host
+        (c/su
+         ; dependencies, only install if not present
+         ; erlang
+         (when-not (try+
+                    (c/exec :erl :-eval "erlang:display (erlang:system_info (otp_release)), halt () ." :-noshell)
+                    true
+                    (catch [] _
+                      false))
+           (debian/install [:build-essential :autoconf :m4 :libncurses-dev :libwxgtk3.2-dev :libwxgtk-webview3.2-dev :libgl1-mesa-dev :libglu1-mesa-dev :libpng-dev :libssh-dev :unixodbc-dev :xsltproc :fop :libxml2-utils
+                            :wget])
+
+           (cu/install-archive! "https://github.com/erlang/otp/releases/download/OTP-25.3.2.8/otp_src_25.3.2.8.tar.gz" "/root/erlang")
+           (c/cd "/root/erlang"
+                 (c/exec "ERL_TOP=/root/erlang" "./configure")
+                 (c/exec "ERL_TOP=/root/erlang" :make)
+                 (c/exec "ERL_TOP=/root/erlang" :make :install)))
+         (info "Erlang version: "
+               (c/exec :erl :-eval "erlang:display (erlang:system_info (otp_release)), halt () ." :-noshell))
+
+         ; elixir
+         (when-not (try+
+                    (c/exec run-elixir :elixir :-v)
+                    true
+                    (catch [] _
+                      false))
+
+           (cu/install-archive! "https://github.com/elixir-lang/elixir/archive/refs/tags/v1.15.7.tar.gz" "/root/elixir")
+           (c/cd "/root/elixir"
+                 (c/exec :make))
+           (run-elixir-sh))
+         (info "Elixir version: "
+               (c/exec run-elixir :elixir :-v))
+
+         ; ElectricSQL
+         (when (not (try+
+                     (c/exec bin-env run-elixir bin :restart)
+                     true
+                     (catch [] _
+                       false)))
+           (debian/install [:git])
+           (c/exec :rm :-rf install-dir)
+           (c/exec :git :clone "https://github.com/electric-sql/electric.git" install-dir)
+           (c/cd (str install-dir "/components/electric")
+                 (c/exec run-elixir :mix :deps.get)
+                 (c/exec run-elixir :mix :compile)
+                 (c/exec (c/env {:MIX_ENV :prod})
+                         run-elixir :mix :release)
+                 (c/exec bin-env run-elixir bin :daemon)
+                 (u/sleep 1000)))
+         (info "ElectricSQL pid: "
+               (c/exec bin-env run-elixir bin :pid))
+
+         ; create and electrify table
+         (insure-psql)
+         (c/exec :psql :-d connection-url
+                 :-c "CREATE TABLE public.lww_registers (key integer PRIMARY KEY, value integer);")
+         (c/exec :psql :-d connection-url
+                 :-c "ALTER TABLE public.lww_registers ENABLE ELECTRIC;")
+         (info "ElectricSQL tables: "
+               (c/exec :psql :-d connection-url
+                       :-c "\\dt")))))
+
 (def opt-spec
   "Specifies CLI options."
   [[nil "--nodes NODE_LIST" (str "Must be " host)
-    :default [host]]])
+    :default [host]]
+   [nil "--teardown" "If set, tears down ElectricSQL."]
+   [nil "--delete"   "If set, deletes ElectricSQL."]
+   [nil "--setup"    "If set, sets up, installing if necessary, and starts ElectricSQL."]])
 
 (defn opt-fn
   "Transforms CLI options before execution."
@@ -39,80 +191,15 @@
 
 (defn run-fn
   "Body of command."
-  [opts]
-  (info (pr-str opts))
-  (info "Installing ElectricSQL on " host)
-  (c/on host
-        (c/su
-         ; stop and cleanup any existing
-         (u/meh
-          (c/exec (str install-dir "/components/electric/_build/prod/rel/electric/bin/electric") :stop)
-          (c/exec :rm :-rf install-dir))
+  [{:keys [options]}]
+  (when (:teardown options)
+    (teardown options))
 
-         ; dependencies, only install if not present
-         ; use `asdf` shims, it's quick & dirty
-         (when-not (try+
-                    (c/exec :asdf :info)
-                    true
-                    (catch [] _
-                      false))
-           (deb/install [:curl :git])
-           (c/exec :git :clone "https://github.com/asdf-vm/asdf.git" "~/.asdf" :--branch "v0.13.1")
-           (c/exec :chmod :u+x "~/.asdf/asdf.sh" "~/.asdf/completions/asdf.bash")
-           (c/exec :echo ". \"$HOME/.asdf/asdf.sh\""               :>> "~/.bashrc")
-           (c/exec :echo ". \"$HOME/.asdf/completions/asdf.bash\"" :>> "~/.bashrc")
-           (info
-            (c/exec :asdf :info)))
+  (when (:delete options)
+    (delete options))
 
-         ; erlang
-         (when-not (try+
-                    (c/exec "erl -eval 'erlang:display (erlang:system_info (otp_release)), halt () .'  -noshell")
-                    true
-                    (catch [] _
-                      false))
-           (deb/install [:build-essential :autoconf :m4 :libncurses-dev :libwxgtk3.2-dev :libwxgtk-webview3.2-dev :libgl1-mesa-dev :libglu1-mesa-dev :libpng-dev :libssh-dev :unixodbc-dev :xsltproc :fop :libxml2-utils])
-           (c/exec :asdf :plugin :add :erlang "https://github.com/asdf-vm/asdf-erlang.git")
-           (c/exec :asdf :install :erlang :25.3.2.8)
-           (c/exec :asdf :global  :erlang :25.3.2.8)
-           (info
-            (c/exec "erl -eval 'erlang:display (erlang:system_info (otp_release)), halt () .'  -noshell")))
-
-         ; elixir
-         (when-not (try+
-                    (c/exec :elixir :-v)
-                    true
-                    (catch [] _
-                      false))
-           (deb/install [:unzip])
-           (c/exec :asdf :plugin :add :elixir "https://github.com/asdf-vm/asdf-elixir.git")
-           (c/exec :asdf :install :elixir :1.15.7-otp-25)
-           (c/exec :asdf :global  :elixir :1.15.7-otp-25)
-           (info
-            (c/exec :elixir :-v)))
-
-         ; always (re)install ElectricSQL
-         (c/exec :git :clone "https://github.com/electric-sql/electric.git" install-dir)
-         (c/cd (str install-dir "/components/electric")
-               (c/exec :asdf :local :erlang :25.3.2.8)
-               (c/exec :asdf :local :elixir :1.15.7-otp-25)
-               (c/exec :mix :deps.get)
-               (c/exec :mix :compile)
-               (c/exec (c/env {:MIX_ENV :prod})
-                       :mix :release)
-               (c/exec (c/env {:DATABASE_URL           postgresql/database-url
-                               :LOGICAL_PUBLISHER_HOST host
-                               :PG_PROXY_PORT          pg-proxy-port
-                               :PG_PROXY_PASSWORD      pg-proxy-password
-                               :AUTH_MODE              :insecure})
-                       (str install-dir "/components/electric/_build/prod/rel/electric/bin/electric") :daemon)
-               (info "ElectricSQL pid: "
-                     (c/exec (str install-dir "/components/electric/_build/prod/rel/electric/bin/electric") :pid))))
-        (info "Installed")))
-
-;; TODO
-;; interact through proxy to create table, etc
-;; CREATE TABLE public.lww_registers (key integer PRIMARY KEY, value integer);
-;; ALTER TABLE lww_registers ENABLE ELECTRIC;
+  (when (:setup options)
+    (setup options)))
 
 (def command
   {"electricsql"
--- src/causal/postgresql.clj
+++ src/causal/postgresql.clj
@@ -4,7 +4,9 @@
             [jepsen
              [control :as c]
              [util :as u]]
-            [jepsen.os.debian :as deb]))
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as deb]
+            [slingshot.slingshot :refer [try+]]))
 
 (def package
   "Package name in repository."
@@ -12,7 +14,7 @@
 
 (def service
   "systemd service name."
-  "postgresql@16-main")
+  "postgresql")
 
 (def host
   "Name of host machine for PostgreSQL."
@@ -26,14 +28,115 @@
   "`electric` role password."
   "electric")
 
-(def database-url
+(def connection-url
   "PostgreSQL connection URI."
   (str "postgresql://" electric-user ":" electric-password "@" host "/electric"))
+
+(defn insure-repo
+  "Insures PostgreSQL repository is installed.
+   Assumes `c/on c/su`."
+  []
+  (when-not (cu/file? "/etc/apt/sources.list.d/pgdg.list")
+    ; add key, repository, and update
+    (u/meh  ; key may already exist, if real failure, will fail in apt update
+     (c/exec :wget :--quiet :-O :- "https://www.postgresql.org/media/keys/ACCC4CF8.asc" :| :sudo :apt-key :add :-))
+    (deb/add-repo! :pgdg "deb https://apt.postgresql.org/pub/repos/apt bookworm-pgdg main")
+    (deb/update!)))
+
+(defn teardown
+  "Teardown PostgreSQL."
+  [_opts]
+  (info "Tearing down PostgreSQL")
+  (c/on host
+        (c/su  ; tests may have stopped/killed database
+         (when (try+
+                (c/exec :systemctl :restart service)
+                true
+                (catch [] _
+                  false))
+           (c/exec :su :- :postgres :-c
+                   "dropdb --force --if-exists electric")
+           (c/exec :su :- :postgres :-c
+                   "dropuser --if-exists electric")
+           (info "PostgreSQL databases: "
+                 (c/exec :su :- :postgres :-c
+                         "psql -U postgres -c '\\dd';"))
+           (info "PostgreSQL roles: "
+                 (c/exec :su :- :postgres :-c
+                         "psql -U postgres -c '\\du';"))
+
+           (c/exec :systemctl :stop service)))))
+
+(defn delete
+  "Delete PostgreSQL."
+  [_opts]
+  (info "Deleting PostgreSQL")
+  (c/on host
+        (c/su
+         (u/meh
+          (c/exec :systemctl :stop service))
+
+         (u/meh
+          (deb/uninstall! [package]))
+
+         (c/exec :rm :-rf
+                 "/var/lib/postgresql*"
+                 "/var/log/postgresql*"
+                 "/etc/postgresql*"))))
+
+(defn setup
+  "Sets up, installing if necessary, and starts PostgreSQL."
+  [_opts]
+  (info "Setting up PostgreSQL")
+  (c/on host
+        (c/su
+         (when (not (deb/installed? package))
+           ; dependencies
+           (deb/install [:lsb-release :wget :gpg])
+
+           ; add key, repository, and package
+           (insure-repo)
+           (deb/install [package])
+
+           ; start service
+           (c/exec :systemctl :start  service)
+
+           ; enable logical replication
+           (c/exec :su :- :postgres :-c
+                   "psql -U postgres -c 'ALTER SYSTEM SET wal_level = logical';")
+           (c/exec :systemctl :restart service)
+           (info
+            (c/exec :su :- :postgres :-c
+                    "psql -U postgres -c 'show wal_level';"))
+
+           ; configure access
+           (c/exec :echo "listen_addresses = '*'"
+                   :>> "/etc/postgresql/16/main/postgresql.conf")
+           (c/exec :echo "host all electric all scram-sha-256"
+                   :>> "/etc/postgresql/16/main/pg_hba.conf")
+           (c/exec :systemctl :restart service))
+
+         (c/exec :systemctl :restart service)
+
+         ; create electric role, database owned by electric
+         (c/exec :su :- :postgres :-c
+                 "psql -U postgres -c \"CREATE ROLE electric WITH LOGIN PASSWORD 'electric' SUPERUSER;\"")
+         (c/exec :su :- :postgres :-c
+                 "createdb -O electric electric")
+         (info "PostgreSQL databases: "
+               (c/exec :su :- :postgres :-c
+                       "psql -U postgres -c '\\dd';"))
+         (info "PostgreSQL roles: "
+               (c/exec :su :- :postgres :-c
+                       "psql -U postgres -c '\\du';")))))
 
 (def opt-spec
   "Specifies CLI options."
   [[nil "--nodes NODE_LIST" (str "Must be " host)
-    :default [host]]])
+    :default [host]]
+   [nil "--teardown" "If set, tears down PostgreSQL."]
+   [nil "--delete"   "If set, deletes PostgreSQL."]
+   [nil "--setup"    "If set, sets up, installing if necessary, and starts PostgreSQL."]])
 
 (defn opt-fn
   "Transforms CLI options before execution."
@@ -44,64 +147,20 @@
 
 (defn run-fn
   "Body of command."
-  [_opts]
-  (info "Installing PostgreSQL on " host)
-  (c/on host
-        (c/su
-         ; dependencies
-         (deb/install [:lsb-release :wget :gpg])
+  [{:keys [options]}]
+  (when (:teardown options)
+    (teardown options))
 
-         ; stop and cleanup any existing
-         (u/meh
-          (c/exec :systemctl :stop service)
-          (deb/uninstall! [package])
-          (c/exec :rm :-rf
-                  "/var/lib/postgresql*"
-                  "/var/log/postgresql*"
-                  "/etc/postgresql*"))
+  (when (:delete options)
+    (delete options))
 
-         ; add key, repository, and package
-         (u/meh  ; key may already exist, if real failure, will fail in apt update
-          (c/exec :wget :--quiet :-O :- "https://www.postgresql.org/media/keys/ACCC4CF8.asc" :| :sudo :apt-key :add :-))
-         (deb/add-repo! :pgdg "deb https://apt.postgresql.org/pub/repos/apt bookworm-pgdg main")
-         (deb/update!)
-         (deb/install [package])
-
-         ; start
-         (c/exec :systemctl :restart service)
-
-         ; configure access
-         (c/exec :eco "listen_addresses = '*'"
-                 :>> "/etc/postgresql/16/main/postgresql.conf")
-         (c/exec :eco "host    all             electric        all                     scram-sha-256"
-                 :>> "/etc/postgresql/16/main/pg_hba.conf")
-
-
-         ; enable logical replication
-         (c/exec :su :- :postgres :-c
-                 "psql -U postgres -c 'ALTER SYSTEM SET wal_level = logical'")
-         (c/exec :systemctl :restart service)
-         (info
-          (c/exec :su :- :postgres :-c
-                  "psql -U postgres -c 'show wal_level'"))
-
-         ; create electric role
-         (c/exec :su :- :postgres :-c
-                 "psql -U postgres -c \"CREATE ROLE electric WITH LOGIN PASSWORD 'electric' SUPERUSER;\"")
-
-         ; create database owned by electric
-         (c/exec :su :- :postgres :-c
-                 "createdb -O electric electric")
-
-         ;; TODO move to electricsql
-         ; create table
-         (c/exec :su :- :postgres :-c
-                 "psql -U postgres -c \"CREATE TABLE public.lww_registers (key integer PRIMARY KEY, value integer);\""))
-        (info "Installed")))
+  (when (:setup options)
+    (setup options)))
 
 (def command
+  "Jepsen CLI command spec."
   {"postgresql"
    {:opt-spec opt-spec
     :opt-fn opt-fn
-    :usage (str "Installs PostgreSQL on " host ".")
+    :usage (str "Setup, teardown, or delete PostgreSQL on " host ".")
     :run run-fn}})
