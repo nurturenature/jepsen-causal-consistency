3993393b55ccb0c751a10da27729406b0eb9c996
nurturenature <jeff.suttor@earthlink.net>
2024-01-04 15:36:06 -0800

    more realistic docker, smaller images, longer health checks

--- docker/setup/electric-setup.sh
+++ docker/setup/electric-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # commands may fail if table already exists
-psql -d postgresql://postgres:proxy_password@electric:65432 -c 'CREATE TABLE public.lww_registers (key integer PRIMARY KEY, value integer);'
+psql -d postgresql://postgres:proxy_password@electric:65432 -c 'CREATE TABLE public.lww_registers (k integer PRIMARY KEY, v integer);'
 psql -d postgresql://postgres:proxy_password@electric:65432 -c 'ALTER TABLE public.lww_registers ENABLE ELECTRIC;'
 
 # insure table is empty, may haven been pre-existing
