9a4a3f6c5920a4fc3b68984276887cc092259544
nurturenature <jeff.suttor@earthlink.net>
2024-03-25 13:10:04 -0700

    docker was aspirational, not real

--- docker/setup/electric-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# commands may fail if table already exists
-psql -d postgresql://postgres:proxy_password@electric:65432 -c 'CREATE TABLE public.lww_registers (k integer PRIMARY KEY, v integer);'
-psql -d postgresql://postgres:proxy_password@electric:65432 -c 'ALTER TABLE public.lww_registers ENABLE ELECTRIC;'
-
-# insure table is empty, may haven been pre-existing
-psql -d postgresql://postgres:proxy_password@electric:65432 -c 'DELETE FROM public.lww_registers;'
