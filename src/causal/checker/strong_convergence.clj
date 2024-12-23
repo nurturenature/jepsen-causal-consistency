1befecbda0b9648e1a09f3922addde2c467db343
nurturenature <jeff.suttor@earthlink.net>
2024-12-22 18:44:04 -0800

    add default opts to final-reads checker params

--- src/causal/checker/strong_convergence.clj
+++ src/causal/checker/strong_convergence.clj
@@ -4,7 +4,8 @@
             [clojure.set :as set]
             [jepsen
              [checker :as checker]
-             [history :as h]]))
+             [history :as h]
+             [store :as store]]))
 
 (defn final-reads
   "Do `:final-read? true` reads strongly converge?
@@ -12,71 +13,76 @@
      - final read from all nodes
      - final read values the same for all nodes
      - all read values were actually written, :ok or :info"
-  []
+  [defaults]
   (reify checker/Checker
     (check [_this {:keys [nodes] :as _test} history opts]
-      (let [nodes           (->> nodes (into (sorted-set)))
-            history         (->> history
-                                 h/client-ops)
+      (let [opts             (merge defaults opts)
+            opts             (update opts :directory (fn [old]
+                                                       (if (nil? old)
+                                                         nil
+                                                         (store/path test [old]))))
+            nodes            (->> nodes (into (sorted-set)))
+            history          (->> history
+                                  h/client-ops)
             history-possible (->> history
                                   h/possible)
-            history-oks     (->> history
-                                 h/oks)
-            history-reads   (->> history-oks
-                                 (h/filter :final-read?))
-            write-index     (cc-g/write-index history-possible)
+            history-oks      (->> history
+                                  h/oks)
+            history-reads    (->> history-oks
+                                  (h/filter :final-read?))
+            write-index      (cc-g/write-index history-possible)
 
             ; {k {v #{node}}}
-            summary         (->> history-reads
-                                 (reduce (fn [summary {:keys [node value] :as _op}]
-                                           (->> value
-                                                (reduce (fn [summary [_f k v]]
-                                                          (update-in summary [k v] set/union #{node}))
-                                                        summary)))
-                                         nil))
+            summary          (->> history-reads
+                                  (reduce (fn [summary {:keys [node value] :as _op}]
+                                            (->> value
+                                                 (reduce (fn [summary [_f k v]]
+                                                           (update-in summary [k v] set/union #{node}))
+                                                         summary)))
+                                          nil))
 
             ; {k #{node}}
-            missing-nodes   (->> summary
-                                 (reduce (fn [missing-nodes [k v->nodes]]
-                                           (let [read-nodes (->> v->nodes
-                                                                 vals
-                                                                 (apply set/union))
-                                                 missing    (set/difference nodes read-nodes)]
-                                             (if (seq missing)
-                                               (update missing-nodes k set/union missing)
-                                               missing-nodes)))
-                                         (sorted-map)))
+            missing-nodes    (->> summary
+                                  (reduce (fn [missing-nodes [k v->nodes]]
+                                            (let [read-nodes (->> v->nodes
+                                                                  vals
+                                                                  (apply set/union))
+                                                  missing    (set/difference nodes read-nodes)]
+                                              (if (seq missing)
+                                                (update missing-nodes k set/union missing)
+                                                missing-nodes)))
+                                          (sorted-map)))
 
             ; {k {[node] v}} sorted by k, sorted by node->v, with nodes sorted
-            divergent-reads (->> summary ; {k {v #{node}}}
+            divergent-reads  (->> summary ; {k {v #{node}}}
                                  ; filter to k's with multiple {v #{node}}
-                                 (filter (fn [[_k v->nodes]]
-                                           (not= 1 (count v->nodes))))
+                                  (filter (fn [[_k v->nodes]]
+                                            (not= 1 (count v->nodes))))
                                  ; reverse map of v->nodes to nodes->v
-                                 (map (fn [[k v->nodes]]
-                                        [k (->> (zipmap (map vec (map sort (vals v->nodes))) (keys v->nodes))
-                                                (into (sorted-map-by (fn [x y] (compare (first x) (first y))))))]))
-                                 (into (sorted-map)))
+                                  (map (fn [[k v->nodes]]
+                                         [k (->> (zipmap (map vec (map sort (vals v->nodes))) (keys v->nodes))
+                                                 (into (sorted-map-by (fn [x y] (compare (first x) (first y))))))]))
+                                  (into (sorted-map)))
 
-            invalid-reads   (->> summary
-                                 (reduce (fn [invalid-reads [k v->nodes]]
-                                           (->> v->nodes
-                                                (reduce (fn [invalid-reads [v nodes]]
-                                                          (let [v (last v)]
-                                                            (cond
+            invalid-reads    (->> summary
+                                  (reduce (fn [invalid-reads [k v->nodes]]
+                                            (->> v->nodes
+                                                 (reduce (fn [invalid-reads [v nodes]]
+                                                           (let [v (last v)]
+                                                             (cond
                                                               ; nil read
-                                                              (nil? v)
-                                                              invalid-reads
+                                                               (nil? v)
+                                                               invalid-reads
 
                                                               ; read value not in write index
-                                                              (nil? (get write-index [k v]))
-                                                              (update-in invalid-reads [k v] set/union nodes)
+                                                               (nil? (get write-index [k v]))
+                                                               (update-in invalid-reads [k v] set/union nodes)
 
                                                               ; valid read
-                                                              :else
-                                                              invalid-reads)))
-                                                        invalid-reads)))
-                                         (sorted-map)))]
+                                                               :else
+                                                               invalid-reads)))
+                                                         invalid-reads)))
+                                          (sorted-map)))]
 
         ; visualize transactions for keys with divergent reads, 3 keys with smallest # total values
         (when (seq divergent-reads)
