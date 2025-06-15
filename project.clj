bfdc801b9fb8c351de157bdadc579572c9aec8bb
nurturenature <jeff.suttor@earthlink.net>
2025-06-14 20:29:30 -0700

    update Clojure 1.12.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/nurturenature/jepsen-causal-consistency"
   :license {:name "Apache License Version 2.0, January 2004"
             :url "http://www.apache.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [jepsen "0.3.9"]
                  ;; TODO: PR elle or revert
                  ;; [elle "0.2.2-SNAPSHOT"]
